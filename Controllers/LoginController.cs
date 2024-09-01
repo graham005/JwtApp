7ffb1ac764d803f8711af37cc58a5f7c4a1ab532
Enoch Graham <enochgraham003@outlook.com>
2024-09-01 13:53:49 +0300

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using JwtApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if (user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User Not Found");
+        }
+
+        private string Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(o => o.Username.ToLower() ==
+                userLogin.Username.ToLower() && o.Password == userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+            return null;
+        }
+
+        private string Generate(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
